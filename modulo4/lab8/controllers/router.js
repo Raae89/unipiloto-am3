142424e03f2bfb42b1f94cfd5a80af1f54f9490f
Ricardo <ricardo.andres.ardila@gmail.com>
2015-10-24 13:07:21 -0500

    Cordova
    Muchos temas

---
+++ modulo4/lab8/controllers/router.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.render('index', {'title' : 'Lab8'});
+});
+
+router.get('/contact', function(req, res){
+	res.render('contact', {});
+});
+
+
+module.exports = router;
